13b53e6d7b7dc8775ee519fcfef253754959a1a1
SushiDeliveryGuy <s4k3@arizona.edu>
2023-04-17 14:19:02 -0700

    SPI code from lecture

--- Lab5_Carpenter_Kerns_Hayes/src/spi.cpp
+++ Lab5_Carpenter_Kerns_Hayes/src/spi.cpp
@@ -1,75 +1,39 @@
-#include "sevensegment.h"
+
+#include <Arduino.h>
+#include "SPI.h"
+#include <avr/interrupt.h>
 #include <avr/io.h>
-#include <util/delay.h>
 
-void initSevenSegment(){
-    //Set data directions for ports C0-C6
-DDRC |= (1<< PORTC0) | (1<<PORTC1) | (1<<PORTC2) | 
-        (1<<PORTC3) | (1<<PORTC4) | (1<<PORTC5) | 
-        (1<<PORTC6);
+#define DDR_SPI DDRB // Data Direction Register on ATMEGA2560 for SPI is DDRB
+#define DD_SS DDB0  // SS Chip Select data direction bit B0 of ATMEGA2560 is DDB0
+#define DD_SCK DDB1  // Clock pin connection data direction bit B1 on ATMEGA2560 is DDB1
+#define DD_MOSI DDB2 // MOSI pin datadirection on ATMEGA2560 is DDB0
+#define SPI_PORT PORTB // PortB for SPI on ATMEGA2560 is PORTB
+#define SPI_SS_BIT PORTB0 // Port B register Bit B0 of Chip Select on ATMEGA2560 is PORTB0
 
-}
+#define wait_for_complete while(!(SPSR & (1 << SPIF)));
 
-void displayNum(unsigned int num){
-    switch (num) {
 
-        case 0:
-            PORTC |= (1<< PORTC0) | (1<<PORTC1) | (1<<PORTC2) | 
-            (1<<PORTC3) | (1<<PORTC4) | (1<<PORTC5); 
-            PORTC &=  ~(1<<PORTC6);
-        break;
-        case 1:
-            PORTC |= (1<<PORTC1) | (1<<PORTC2); 
-         
-            PORTC &=  ~(1<< PORTC0) & 
-            ~(1<<PORTC3) & ~(1<<PORTC4) & ~(1<<PORTC5) & ~(1<<PORTC6);
-        break;
-         case 2:
-            PORTC |= (1 << PORTC0) | (1 << PORTC1) | 
-                     (1 << PORTC4) | (1 << PORTC6) | 
-                     (1 << PORTC3);
-            PORTC &= ~(1 << PORTC2) & ~(1 << PORTC5);
-        break;
-        case 3:
-            PORTC |= (1 << PORTC0) | (1 << PORTC1) | 
-                     (1 << PORTC2) | (1 << PORTC3) | 
-                     (1 << PORTC6);
-            PORTC &= ~(1 << PORTC4) & ~(1 << PORTC5);
-        break;
-        case 4:
-            PORTC |= (1 << PORTC1) | (1 << PORTC2) |
-                     (1 << PORTC5) | (1 << PORTC6);
-            PORTC &= ~(1 << PORTC0) & ~(1 << PORTC3) & ~(1 << PORTC4);
-        break;
-        case 5:
-            PORTC |= (1 << PORTC0) | (1 << PORTC2) |
-                     (1 << PORTC3) | (1 << PORTC5) |
-                     (1 << PORTC6);
-            PORTC &= ~(1 << PORTC1) & ~(1 << PORTC4);
-        break;
-        case 6:
-            PORTC &= ~(1 << PORTC1);
-            PORTC |= (1 << PORTC0) | (1 << PORTC2) | (1 << PORTC3);
-            PORTC |= (1 << PORTC4) | (1 << PORTC5) | (1 << PORTC6);
-        break;
-        case 7:
-            PORTC |= (1<< PORTC0) | (1<<PORTC1) | (1<<PORTC2);
-         
-            PORTC &=  ~(1<<PORTC3) & ~(1<<PORTC4) & ~(1<<PORTC5) & ~(1<<PORTC6);
-        break;
-        case 8:
-            PORTC |= (1<< PORTC0) | (1<<PORTC1) | (1<<PORTC2) | 
-            (1<<PORTC3) | (1<<PORTC4) | (1<<PORTC5) | (1<<PORTC6);
-        break;
-        case 9:
-            PORTC |= (1<< PORTC0) | (1<<PORTC1) | (1<<PORTC2) | 
-            (1<<PORTC3) | (1<<PORTC5) | (1<<PORTC6);
-         
-            PORTC &=  ~(1<<PORTC4);
-        break;
-        default:
-        PORTC &=  ~(1<< PORTC0) & ~(1<< PORTC1) & ~(1<< PORTC2) &
-            ~(1<<PORTC3) & ~(1<<PORTC4) & ~(1<<PORTC5) & ~(1<<PORTC6);
-        break;
+void SPI_MASTER_Init() {
+    // set MOSI,SCK,and SS direction to outputs
+    DDR_SPI = (1 << DD_MOSI)| (1 << DD_SCK) | (1 << DD_SS);
+
+    // note this program does not use MISO line. IT only writes to device
+
+    // set SS high initially (chip select off)
+    SPI_PORT  |= (1 << SPI_SS_BIT);
+
+    // enable SPI, master mode, CPOL, CPHA, default clock and fosc/128
+    // datasheet says sample on rising edge CPOL = 1 CPHA =1
+    SPCR  |= (1 <<SPE)| (1 << MSTR) | (1 << CPOL) | (1 << CPHA ) | (1 << SPR1) | (1 << SPR0);
+
     }
-}
+
+    void write_execute(unsigned char CMD, unsigned char data) {
+        SPI_PORT &= ~(1 << SPI_SS_BIT);  // enable chip select bit to begin SPI frame
+        SPDR = CMD; // load the CMD address into register
+        wait_for_complete; // wait for flag to raise
+        SPDR = data; // load the data into register
+        wait_for_complete; // wait for flag to raise
+        SPI_PORT |= (1 << SPI_SS_BIT); // disable chip select to end SPI frame
+    }
