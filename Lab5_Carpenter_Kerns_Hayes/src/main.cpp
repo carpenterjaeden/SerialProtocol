6ec621ed61b8e7ca6dfed57674dc55a702acde5c
SushiDeliveryGuy <s4k3@arizona.edu>
2023-04-17 15:53:27 -0700

    MainFile header

--- Lab5_Carpenter_Kerns_Hayes/src/main.cpp
+++ Lab5_Carpenter_Kerns_Hayes/src/main.cpp
@@ -1,14 +1,29 @@
-// Author: Jaeden Carpenter
-// Net ID: carpenterjaeden
-// Date: 3/20/23
-// Assignment: Lab 3
+// Author: Jaeden Carpenter, Sam Kerns, Malcolm Hayes
+// Net ID: carpenterjaeden, s4k3, malcolmhayes
+// Date: 5/1/23
+// Assignment: Lab 5
 //----------------------------------------------------------------------//
 
-// Description: This file contains a programmatic overall description of the
-// program. It should never contain assignments to special function registers
-// for the exception key one-line code such as checking the state of the pin.
+// Description: In this lab, you will work in a ** team ** of 3-4 people.
 //
-// Requirements:
+// You will use the MPU 6050 accelerometer module and I2C serial
+// communication protocol to read and print the measured X, Y, and Z data
+// accelerometer from the module. You will also detect changes in either Y
+// and Z axis rotation above a predetermined threshold level (~ 45 degrees
+// for each axis). This threshold level will determine the state of the
+// 8x8 LED matrix display and whether a buzzer alarm sounds off.
+// You will interface with the 8 x 8 LED matrix using the SPI
+// communication protocol. While the MPU 6050 accelerometer is below the
+// threshold level that you determine, you will display a smiley face on
+// the 8 x 8 LED matrix. When the MPU 6050 accelerometer goes above the
+// threshold level, the smiley face turns to a frowny face on the 8 x 8
+// led matrix display.
+//
+// You will also implement a buzzer alarm that produces a chirping sound
+// when the accelerometer sensor goes above a certain threshold value.
+//
+// A push button switch will silence the piezo alarm.
+//
 //----------------------------------------------------------------------//
 
 
