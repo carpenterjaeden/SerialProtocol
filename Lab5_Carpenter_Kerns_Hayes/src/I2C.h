00f9506f5f0d38684f3e98637eae50eb2c94b41e
carpenterjaeden <112436035+carpenterjaeden@users.noreply.github.com>
2023-04-17 14:37:40 -0700

    Update I2C.h

---
+++ Lab5_Carpenter_Kerns_Hayes/src/I2C.h
@@ -0,0 +1,9 @@
+#ifndef I2C_H
+#define I2C_H
+
+void InitI2C();
+void StartI2C_Trans(unsigned char SLA);
+void StopI2C_Trans();
+void Read_from(unsigned char SLA, unsigned char MEMADDRESS);
+
+#endif
